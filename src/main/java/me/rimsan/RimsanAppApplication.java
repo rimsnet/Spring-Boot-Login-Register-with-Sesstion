2cd89aef4f0f4776517d4092ec56349730392615
Rimsan <rimsnet@gmail.com>
2021-02-20 17:45:31 +0800

    initial

---
+++ src/main/java/me/rimsan/RimsanAppApplication.java
@@ -0,0 +1,13 @@
+package me.rimsan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimsanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimsanAppApplication.class, args);
+	}
+
+}
